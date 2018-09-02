090e3dc327b07c4c3200c5393c7faa537738141a
Felix Meissner <felixmeissner@me.com>
2018-09-02 16:10:49 +0200

    Completed the module 2

--- src/app.js
+++ src/app.js
@@ -5,16 +5,37 @@ const express = require('express');
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/public'));
 
+const accountData = fs.readFileSync("src/json/accounts.json", { encoding: 'UTF8' });
+const accounts = JSON.parse(accountData);
 
-app.get('/',function(req,res) {
-    res.render('index',{ title: 'Index' });
+const userData = fs.readFileSync("src/json/users.json", { encoding: 'UTF8' });
+const users = JSON.parse(userData);
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Account Summary', accounts: accounts });
+});
+
+app.get('/savings', function (req, res) {
+    res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', function (req, res) {
+    res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', function (req, res) {
+    res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', function (req,res) {
+    res.render('profile', { user: users[0] })
 })
 
-app.listen(3000,function() {
+app.listen(3000, function () {
     console.log('PS Project Running on port 3000!');
 });
